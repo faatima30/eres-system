60da1045f0530a18c3e6626fb1463cc5e978003a
Faatima Abdirahmaan Ali <faatimaapdirahmaanali@gmail.com>
2024-11-16 11:22:24 +0300

    added mysql connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
-// index.js
 const express = require("express");
+const pool = require("./db");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Node.js with Express!");
+pool.getConnection((connection, err) => {
+  if (connection) {
+    console.log("Connected to the database!");
+    connection.release();
+  } else {
+    console.error("Database connection failed:", err.message);
+  }
 });
+
+app.get("/", async (req, res) => {
+    try {
+      // Query the users table
+      const [rows] = await pool.query("SELECT * FROM users");
+      res.json(rows); // Send the result as JSON
+    } catch (error) {
+      console.error("Error fetching users:", error.message);
+      res.status(500).send("An error occurred while fetching users.");
+    }
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
